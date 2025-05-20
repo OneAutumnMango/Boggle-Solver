044174cc85665fce26665200616eef31a5654f70
DavidWhittam <124370392+DavidWhittam@users.noreply.github.com>
2025-05-20 22:39:20 +0100

    add argparse

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
+import argparse
 from utils.boggle import Boggle
 from utils.boggle_solver import BoggleSolver
 from utils.dictionary import DictionaryBuilder
 
 def main():
-    boggle = Boggle(size=4)
+    parser = argparse.ArgumentParser(description="Solve a Boggle board.")
+    parser.add_argument("board", type=str, help="Required custom board string (e.g. 'abcdefghijklmnop')")
+    args = parser.parse_args()
+
+    board = args.board.lower()
+    boggle = Boggle(size=4, board_str=board)
+    
     dictionary = DictionaryBuilder().get_or_build()
     solver = BoggleSolver(dictionary, boggle)
     
     print(boggle)
 
     words = solver.solve()
-    print("ALHPABETICAL ORDER")
+    print("\nALPHABETICAL ORDER")
     print(words)
 
     print("\nBY LENGTH")
     print(sorted(words, key=len, reverse=True))
 
     print(f"Found {len(words)} words")
-
 
 
 if __name__ == "__main__":
